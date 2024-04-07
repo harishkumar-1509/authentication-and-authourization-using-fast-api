129036beccb7fc194bbe371c04989c78f5c05d92
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-04-07 22:37:39 +0530

    added security file contents

--- app/services/user.py
+++ app/services/user.py
@@ -1,11 +1,18 @@
 from app.models.user import User
+from app.config.security import hash_password
+from fastapi import HTTPException
 
 async def create_user_account(data, session):
+    
+    user_exist = session.query(User).filter(User.email == data.email).first()
+    if user_exist:
+        raise HTTPException(status_code=400, details="Email already exists!")
+    
     user = User()
     user.name = data.name
     user.email = data.email
     user.mobile_no = data.mobile_no
-    user.password = data.password
+    user.password = hash_password(data.password)
     user.is_active = True
     
     session.add(user)
